fd88174fe34b35b5cf2806e3297eb58174b41afd
Louis Valet <a.louisvalet@gmail.com>
2017-12-02 12:29:58 +0100

    DE LA NERFGE

--- Assets/03_SCRIPTS/MoveableObject.cs
+++ Assets/03_SCRIPTS/MoveableObject.cs
@@ -4,20 +4,46 @@
 public class MoveableObject : MonoBehaviour
 {
 	private Rigidbody m_RigidBody;
-	private CharacterJoint mouseJoint;
+	private CharacterJointDisabler mouseJoint;
 
 	private void Awake()
 	{
 		m_RigidBody = GetComponent<Rigidbody>();
+		mouseJoint = new CharacterJointDisabler();
+
+		mouseJoint.CopyValuesAndDestroyJoint( GetComponent<CharacterJoint>() );
 	}
 
-	public void PickupObject()
+	private void Start()
 	{
+	}
+
+	public void PickupObject( Rigidbody joint )
+	{
+		Vector3 connectdAnchor = Vector3.zero;
+		connectdAnchor.y = joint.transform.position.y;
+
+		transform.position = joint.transform.position - mouseJoint.Anchor;
+		mouseJoint.CreateJoint( gameObject, joint, connectdAnchor );
+		m_RigidBody.isKinematic = false;
+		m_RigidBody.velocity = Vector3.zero;
+		m_RigidBody.angularVelocity = Vector3.zero;
 
 	}
 
-	public void ReleaseObject()
+	public void ReleaseObject( Vector3 mouseVelocity )
 	{
+		mouseJoint.DestroyJoint();
+		m_RigidBody.velocity = Vector3.zero;
+		m_RigidBody.angularVelocity = Vector3.zero;
+		m_RigidBody.AddForce( mouseVelocity * 0.2f, ForceMode.Impulse );
+		//RaycastHit hit;
+		//if ( Physics.Raycast( transform.position, Vector3.down, out hit, 100 ) )
+		//{
+		//	transform.rotation = Quaternion.identity;
+		//	transform.position = hit.point;
 
+
+		//}
 	}
 }
