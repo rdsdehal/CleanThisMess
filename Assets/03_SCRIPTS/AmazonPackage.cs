a0f5e7f4e4b6d90d4f5ef9cc61ec8ed258e0e64b
Louis Valet <a.louisvalet@gmail.com>
2017-12-02 18:09:39 +0100

    meta?

---
+++ Assets/03_SCRIPTS/AmazonPackage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AmazonPackage : MonoBehaviour
+{
+	public GameObject packageContents;
+
+	private void OnCollisionEnter( Collision collision )
+	{
+		packageContents.transform.position = transform.position;
+		packageContents.transform.rotation = Quaternion.identity;
+		packageContents.SetActive( true );
+
+		packageContents.GetComponent<Rigidbody>().AddForce( Vector3.up * 20, ForceMode.Impulse );
+		Destroy( gameObject );
+	}
+}
