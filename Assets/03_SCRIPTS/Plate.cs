5957367c835de6514f3271507da5e2d54168484a
Louis Valet <a.louisvalet@gmail.com>
2017-12-02 16:01:35 +0100

    up prefab

--- Assets/03_SCRIPTS/Plate.cs
+++ Assets/03_SCRIPTS/Plate.cs
@@ -11,7 +11,38 @@ public class Plate : MonoBehaviour
 
 	public PlateState plateState = PlateState.Clean;
 
+	public GameObject dirtyPlate;
+	public GameObject[] food;
 
+	public void Clean()
+	{
+		plateState = PlateState.Clean;
 
+		dirtyPlate.SetActive( false );
+		for ( int i = 0 ; i < food.Length ; i++ )
+		{
+			food[i].SetActive( false );
+		}
+	}
 
+	public void Cook()
+	{
+		if ( plateState == PlateState.Clean )
+		{
+			plateState = PlateState.Full;
+
+			food[Random.Range( 0, food.Length )].SetActive( true );
+		}
+	}
+
+	public void Consume()
+	{
+		plateState = PlateState.Dirty;
+
+		dirtyPlate.SetActive( true );
+		for ( int i = 0 ; i < food.Length ; i++ )
+		{
+			food[i].SetActive( false );
+		}
+	}
 }
---
+++ Assets/03_SCRIPTS/Sink.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Sink : MonoBehaviour
+{
+	private void OnTriggerEnter( Collider other )
+	{
+		var plate = other.GetComponentInParent<Plate>();
+		Debug.Log( "fff" );
+		if ( plate != null )
+		{
+			plate.Clean();
+
+		}
+	}
+}
