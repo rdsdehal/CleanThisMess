68f274d088773a55a6a5c557e645bd98f86e00d4
Sullivan Devroede <sullivan.devroede@gmail.com>
2017-12-02 15:13:23 +0100

    ChildBehaviour

--- Assets/03_SCRIPTS/MoveableObject.cs
+++ Assets/03_SCRIPTS/MoveableObject.cs
@@ -1,52 +1,53 @@
 ﻿using UnityEngine;
 
-[RequireComponent( typeof( Rigidbody ) )]
+[SelectionBase]
+[RequireComponent(typeof(Rigidbody))]
 public class MoveableObject : MonoBehaviour
 {
-	public string objectType;
-	[Range( 0, 1 )]
-	public float mouseVelocityFactor = 1;
-	public Vector3 localAnchor;
-	public bool canBePickedUp;
+    public string objectType;
+    [Range(0, 1)]
+    public float mouseVelocityFactor = 1;
+    public Vector3 localAnchor;
+    public bool canBePickedUp;
 
-	private Rigidbody m_RigidBody;
+    private Rigidbody m_RigidBody;
 
-	private void Awake()
-	{
-		m_RigidBody = GetComponent<Rigidbody>();
-	}
+    private void Awake()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
 
-	public void PickupObject( Rigidbody joint )
-	{
-		m_RigidBody.isKinematic = true;
-		m_RigidBody.useGravity = false;
-		m_RigidBody.velocity = Vector3.zero;
-		m_RigidBody.angularVelocity = Vector3.zero;
-		transform.parent = joint.transform;
+    public void PickupObject(Rigidbody joint)
+    {
+        m_RigidBody.isKinematic = true;
+        m_RigidBody.useGravity = false;
+        m_RigidBody.velocity = Vector3.zero;
+        m_RigidBody.angularVelocity = Vector3.zero;
+        transform.parent = joint.transform;
 
-		transform.localPosition = Vector3.zero;
-	}
+        transform.localPosition = Vector3.zero;
+    }
 
-	public void ReleaseObject( Vector3 mouseVelocity )
-	{
-		m_RigidBody.velocity = Vector3.zero;
-		m_RigidBody.angularVelocity = Vector3.zero;
-		m_RigidBody.isKinematic = false;
-		m_RigidBody.useGravity = true;
-		transform.SetParent( null, true );
+    public void ReleaseObject(Vector3 mouseVelocity)
+    {
+        m_RigidBody.velocity = Vector3.zero;
+        m_RigidBody.angularVelocity = Vector3.zero;
+        m_RigidBody.isKinematic = false;
+        m_RigidBody.useGravity = true;
+        transform.SetParent(null, true);
 
-		m_RigidBody.AddForce( mouseVelocity * mouseVelocityFactor, ForceMode.Impulse );
-	}
+        m_RigidBody.AddForce(mouseVelocity * mouseVelocityFactor, ForceMode.Impulse);
+    }
 
-	public void ReleaseObject( Transform snapPos )
-	{
-		m_RigidBody.velocity = Vector3.zero;
-		m_RigidBody.angularVelocity = Vector3.zero;
-		m_RigidBody.isKinematic = false;
-		m_RigidBody.useGravity = true;
-		transform.SetParent( null, true );
+    public void ReleaseObject(Transform snapPos)
+    {
+        m_RigidBody.velocity = Vector3.zero;
+        m_RigidBody.angularVelocity = Vector3.zero;
+        m_RigidBody.isKinematic = false;
+        m_RigidBody.useGravity = true;
+        transform.SetParent(null, true);
 
-		transform.position = snapPos.position;
-		transform.rotation = snapPos.rotation;
-	}
+        transform.position = snapPos.position;
+        transform.rotation = snapPos.rotation;
+    }
 }
