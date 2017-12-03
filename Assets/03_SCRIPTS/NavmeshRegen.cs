4ab0a21d5d248c85ad9eff92a8aa94db197c66f9
Louis Valet <a.louisvalet@gmail.com>
2017-12-03 11:59:41 +0100

    fe

---
+++ Assets/03_SCRIPTS/NavmeshRegen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavmeshRegen : MonoBehaviour
+{
+	public float regenEverySec;
+	NavMeshSurface surface;
+
+	void Awake()
+	{
+		surface = GetComponent<NavMeshSurface>();
+		StartCoroutine( Regen() );
+	}
+
+	IEnumerator Regen()
+	{
+		WaitForSeconds wait = new WaitForSeconds( regenEverySec );
+
+		while ( true )
+		{
+			surface.BuildNavMesh();
+			yield return wait;
+		}
+	}
+}
