06caabfc74d08afd872efa9780e72a58cdad7dd4
Louis Valet <a.louisvalet@gmail.com>
2017-12-03 10:17:25 +0100

    automatikk

--- Assets/03_SCRIPTS/ObjectsManager.cs
+++ Assets/03_SCRIPTS/ObjectsManager.cs
@@ -5,61 +5,66 @@ using UnityEngine;
 
 public class ObjectsManager : MonoBehaviour
 {
-    public List<Chair> m_ChairList;
-    public List<GameObject> m_ThrowableList;
-    public Chair m_LastChair = null;
+	public List<Chair> m_ChairList;
+	public List<GameObject> m_ThrowableList;
+	public Chair m_LastChair = null;
 
-    public Chair FindChair(GameObject m_Child)
-    {
-        Chair m_Chair = null;
-        List<Chair> m_AvailableChair = m_ChairList.Where(m => CheckChairRotation(m) == true && m.isOccupied == false).ToList();
-        if (m_AvailableChair.Count() > 0)
-        {
-            if (m_LastChair != null)
-            {
-                m_AvailableChair.Remove(m_LastChair);
-            }
-            if (m_AvailableChair.Count() > 0)
-            {
-                m_Chair = m_AvailableChair[Random.Range(0, m_AvailableChair.Count())];
-            }
-        }
-        m_LastChair = m_Chair;
-        return m_Chair;
-    }
+	private void Awake()
+	{
+		m_ThrowableList = FindObjectsOfType<MoveableObject>().Where( m => m.canBePickedUp ).Select( m => m.gameObject ).ToList();
+	}
 
-    public GameObject FindNearestThrowable(GameObject m_Child)
-    {
-        GameObject NearestThrowable = null;
-        float lastDistance = Mathf.Infinity;
-        List<GameObject> m_AvailableToThrow = m_ThrowableList;
-        if (m_LastChair != null)
-        {
-            m_AvailableToThrow.Remove(m_LastChair.gameObject);
-        }
-        foreach (GameObject element in m_AvailableToThrow)
-        {
-            if (element.gameObject.activeSelf == true)
-            {
-                float calculatedDistance = Mathf.Abs(Vector3.Distance(m_Child.transform.position, element.transform.position));
-                if (lastDistance > calculatedDistance)
-                {
-                    lastDistance = calculatedDistance;
-                    NearestThrowable = element;
-                }
-            }
-        }
-        return NearestThrowable;
-    }
+	public Chair FindChair( GameObject m_Child )
+	{
+		Chair m_Chair = null;
+		List<Chair> m_AvailableChair = m_ChairList.Where( m => CheckChairRotation( m ) == true && m.isOccupied == false ).ToList();
+		if ( m_AvailableChair.Count() > 0 )
+		{
+			if ( m_LastChair != null )
+			{
+				m_AvailableChair.Remove( m_LastChair );
+			}
+			if ( m_AvailableChair.Count() > 0 )
+			{
+				m_Chair = m_AvailableChair[Random.Range( 0, m_AvailableChair.Count() )];
+			}
+		}
+		m_LastChair = m_Chair;
+		return m_Chair;
+	}
 
-    public bool CheckChairRotation(Chair m_Chair)
-    {
-        bool m_IsValid = false;
-        if (Vector3.Dot(m_Chair.transform.up, Vector3.up) > 0.8f)
-        {
-            m_IsValid = true;
-        }
-        return m_IsValid;
-    }
+	public GameObject FindNearestThrowable( GameObject m_Child )
+	{
+		GameObject NearestThrowable = null;
+		float lastDistance = Mathf.Infinity;
+		List<GameObject> m_AvailableToThrow = m_ThrowableList;
+		if ( m_LastChair != null )
+		{
+			m_AvailableToThrow.Remove( m_LastChair.gameObject );
+		}
+		foreach ( GameObject element in m_AvailableToThrow )
+		{
+			if ( element.gameObject.activeSelf == true )
+			{
+				float calculatedDistance = Mathf.Abs( Vector3.Distance( m_Child.transform.position, element.transform.position ) );
+				if ( lastDistance > calculatedDistance )
+				{
+					lastDistance = calculatedDistance;
+					NearestThrowable = element;
+				}
+			}
+		}
+		return NearestThrowable;
+	}
+
+	public bool CheckChairRotation( Chair m_Chair )
+	{
+		bool m_IsValid = false;
+		if ( Vector3.Dot( m_Chair.transform.up, Vector3.up ) > 0.8f )
+		{
+			m_IsValid = true;
+		}
+		return m_IsValid;
+	}
 }
 
