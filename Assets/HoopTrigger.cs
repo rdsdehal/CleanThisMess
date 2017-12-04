afffc62e161bd0cf2908d7cec341aedfae1e5a92
Louis Valet <a.louisvalet@gmail.com>
2017-12-04 14:31:06 +0100

    no message

---
+++ Assets/HoopTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopTrigger : MonoBehaviour
+{
+	public ParticleSystem fx;
+	public AudioSource source;
+	public AudioClip clip;
+
+	void OnTriggerEnter( Collider other )
+	{
+		var move = other.GetComponentInParent<MoveableObject>();
+		if ( move && move.objectType == "basketball" )
+		{
+			source.PlayOneShot( clip );
+			fx.Play();
+		}
+	}
+}
