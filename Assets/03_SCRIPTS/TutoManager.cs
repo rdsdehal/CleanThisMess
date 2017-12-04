1a07153f88c11f4a976d3db3638eecae399c1334
Louis Valet <a.louisvalet@gmail.com>
2017-12-04 10:34:27 +0100

    no message

---
+++ Assets/03_SCRIPTS/TutoManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoManager : MonoBehaviour
+{
+	public GameObject tutoPPRoot;
+	int currentslide = 0;
+	bool isOK;
+
+	private void Awake()
+	{
+		Time.timeScale = 0;
+	}
+
+	private void Update()
+	{
+		if ( Input.GetMouseButtonDown( 0 ) )
+		{
+			for ( int i = 0 ; i < tutoPPRoot.transform.childCount ; i++ )
+			{
+				tutoPPRoot.transform.GetChild( i ).gameObject.SetActive( false );
+			}
+			currentslide++;
+
+
+			if ( currentslide < tutoPPRoot.transform.childCount )
+			{
+				tutoPPRoot.transform.GetChild( currentslide ).gameObject.SetActive( true );
+			}
+			else if ( !isOK )
+			{
+				isOK = true;
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
